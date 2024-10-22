3b78d0be741f65ade30e3118f3a10fd35dfcdba8
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-22 12:39:16 +0200

    Add CenterInfo component with basic template and styles

---
+++ frontend/src/app/components/center-info/center-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-center-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './center-info.component.html',
+  styleUrl: './center-info.component.scss'
+})
+export class CenterInfoComponent {
+
+}
