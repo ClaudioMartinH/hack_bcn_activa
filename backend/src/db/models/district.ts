78c51069e71bbc8f8f37e1fde7f5bbd69c2b58b5
ghOdisea <154799410+ghOdisea@users.noreply.github.com>
2024-10-22 15:50:12 +0200

    district interface

--- backend/src/db/models/district.ts
+++ backend/src/db/models/district.ts
@@ -2,8 +2,12 @@ import mongoose from "mongoose";
 
 export interface IDistrict {
       name: string
+      district_code: string
       location: string
-      data: string
+      digital_gap: string
+      educational_center: string
+      employment_situation: string
+      income_per_person: string
       createdAt: Date
       updatedAt: Date
 }
@@ -11,7 +15,10 @@ export interface IDistrict {
 const districtSchema = new mongoose.Schema({
       name: { type: String, required: true },
       location: { type: String, required: true },
-      data: { type: String, required: true },
+      digital_gap: { type: String, required: true },
+      educational_center: { type: String, required: true },
+      employment_situation: { type: String, required: true },
+      income_per_person: { type: String, required: true },
       createdAt: { type: Date, default: Date.now },
       updatedAt: { type: Date, default: null },
 })
