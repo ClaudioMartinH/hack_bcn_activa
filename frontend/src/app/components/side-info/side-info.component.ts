566b3e816eacff9b8cba0fafbd2ed7c6600a2034
Miriam Martin Casas <miriam.martin.casas@gmail.com>
2024-10-22 16:35:11 +0200

    aside details

--- frontend/src/app/components/side-info/side-info.component.ts
+++ frontend/src/app/components/side-info/side-info.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+
 
 @Component({
   selector: 'app-side-info',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './side-info.component.html',
   styleUrl: './side-info.component.scss'
 })
 export class SideInfoComponent {
+
+  sidebarVisible: boolean = false;
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
+
+  isMobile(): boolean {
+    return window.innerWidth < 768;
+  }
+
+  // Escuchar cambios en el tamaño de la pantalla para ajustar el sidebar si cambia el viewport
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (!this.isMobile()) {
+      this.sidebarVisible = true;
+    }
+  }
 
 }
